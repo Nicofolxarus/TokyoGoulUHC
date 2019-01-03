cadf4b7b0c499cf53c3d8725847039d54a4048cd
Re3se <thomasfusxo@gmail.com>
2019-01-03 23:56:19 +0400

    DeathEvent tentative

--- src/fr/falkoyt/DeathEvent.java
+++ src/fr/falkoyt/DeathEvent.java
@@ -1,12 +1,16 @@
 package fr.falkoyt;
 
 
+//import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathEvent implements Listener{
 public void onKill(PlayerDeathEvent e)
 {
+//Player v = (Player) e.getEntity();
+//Player p = (Player) v.getKiller();
+//p.getName();
 //String killer = e.getEntity().getName();
 
 
