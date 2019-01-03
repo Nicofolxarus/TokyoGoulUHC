ac3725ce7f84fa6f271ed5cbf9f9c3482749f26a
Re3se <thomasfusxo@gmail.com>
2019-01-03 23:40:39 +0400

    Scoreboard+DeathEvent

---
+++ src/fr/falkoyt/DeathEvent.java
@@ -0,0 +1,14 @@
+package fr.falkoyt;
+
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathEvent implements Listener{
+public void onKill(PlayerDeathEvent e)
+{
+//String killer = e.getEntity().getName();
+
+
+}
+}
